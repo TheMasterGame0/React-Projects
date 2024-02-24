c317998a0761e9e9caa274c5341535850b05ff0e
TheMasterGame0 <TheMasterGame0Contact@gmail.com>
2024-02-23 23:51:47 -0300

    Back-End Inicial
    Implementação do Back-End responsável pelas telas de tutoriais. Ainda faltam as partes responsáveis pelo login.

---
+++ REACT-HOOKS-CRUD/src/tutoriais/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+# Modelo a ser utilizado para um tutorial
+class Tutorial(models.Model):
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
+    published = models.BooleanField(default=False)
